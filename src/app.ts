9afdf652d1a4c07aa7d91a580be81594fd8dd757
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-13 15:59:00 +1000

    feat(app): initial route hookup, hydration from state and listeners

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,44 @@
-export class App {
+import { PLATFORM } from 'aurelia-pal';
+import { RouterConfiguration, Router } from 'aurelia-router';
+import { AuthorizeStep } from './resources/pipeline-steps/auth-pipeline';
+import { Store, dispatchify, rehydrateFromLocalStorage } from 'aurelia-store';
+import { State } from 'store/state';
+import { Subscription } from 'rxjs';
+import { autoinject } from 'aurelia-framework';
+import { login } from 'store/actions';
 
+@autoinject()
+export class App {
+    private router: Router;
+    private state: State;
+    private subscription: Subscription;
+
+    constructor(private store: Store<State>) {
+        this.subscription = this.store.state.subscribe((state) => {
+            if (state) {
+                this.state = state;
+
+                AuthorizeStep.loggedIn = state.loggedIn;
+            }
+        });
+    }
+
+    // On initial load, rehydrate store from cache middleware
+    attached() {
+        dispatchify(rehydrateFromLocalStorage)('steem_marketplace__giftcard');
+    }
+
+    configureRouter(config: RouterConfiguration, router: Router) {
+        config.title = 'Gift Card Marketplace';
+
+        config.options.pushState = true;
+
+        config.map([
+            { route: [''], name: 'home', title: 'Home', moduleId: PLATFORM.moduleName('./routes/home'), nav: true }
+        ]);
+
+        config.addAuthorizeStep(AuthorizeStep);
+
+        this.router = router;
+    }
 }
