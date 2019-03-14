f9bee6583bc30531cc42bc92ac5cbfab836ea5d7
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-14 17:06:20 +1000

    feat(interfaces): interfaces file and cmc interface typing

---
+++ src/common/interfaces.ts
@@ -0,0 +1,17 @@
+interface CoinMarketCapTickerInterface {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: string;
+    price_usd: string;
+    price_btc: string;
+    '24h_volume_usd': string;
+    market_cap_usd: string;
+    available_supply: string;
+    total_supply: string;
+    max_supply: string | null;
+    percent_change_1h: string;
+    percent_change_24h: string;
+    percent_change_7d: string;
+    last_updated: string;
+}
