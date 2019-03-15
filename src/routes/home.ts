e0d8920ba7ba1b875765788ff171c0aab0edc4a3
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-15 15:05:47 +1000

    feat(home): display featured products section

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,5 +1,12 @@
 import styles from './home.css';
+import { dispatchify, connectTo } from 'aurelia-store';
+import { getFeaturedProducts } from 'store/actions';
 
+@connectTo()
 export class Home {
     private styles = styles;
+
+    async activate() {
+        await dispatchify(getFeaturedProducts)();
+    }
 }
