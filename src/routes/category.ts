54ccb9d52be87382a5e4a93241df8e2486d9c6e8
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-03-20 21:14:01 +1000

    feat(category): get params from url for category screen

--- src/routes/category.ts
+++ src/routes/category.ts
@@ -2,5 +2,11 @@
 // This code is licensed under MIT license (see LICENSE for details)
 
 export class Category {
-    
+    async canActivate(params) {
+        // Pagination offset
+        const page = !params.page ? 1 : parseInt(params.page);
+
+        // Category slug
+        const category = params.category;
+    }
 }
